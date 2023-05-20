9f82c36d5bbc99682562d0e296a10f33fc06c79d
Family <familygw@gmail.com>
2023-05-19 22:06:13 -0300

    webpack configs

--- angular-mfe/webpack.config.js
+++ angular-mfe/webpack.config.js
@@ -1,14 +1,44 @@
-const { share, shareAll, withModuleFederationPlugin } = require("@angular-architects/module-federation/webpack");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const { share, shareAll } = mf;
 
-module.exports = withModuleFederationPlugin({
-  name: "angularMfe",
-  filename: "remoteEntry.js",
-  exposes: {
-    "./AngularMFEModule": "./src/app/angular-mfe/angular-mfe.module",
-    // "./AngularMFEModule": "./src/app/app.module",
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, "tsconfig.json"),
+  [/* mapped paths to share */]);
+
+module.exports = {
+  output: {
+    uniqueName: "taxGTPWui",
+    publicPath: "auto",
+    scriptType: "text/javascript",
   },
-  shared: {
-    ...shareAll({ singleton: true, strictVersion: false, requiredVersion: "auto" }),
-    ...share({ "zone.js": { singleton: true, strictVersion: false, requiredVersion: "auto " } })
+  optimization: {
+    runtimeChunk: false
   },
-});
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    }
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "taxGTPWui",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MFEMyTasksModule": "./src/app/mfe-modules/mfe-my-tasks.module.ts",
+        "./MFEMyTasksComponent": "./src/app/mfe-modules/mfe-my-tasks/mfe-my-tasks-sa.component.ts",
+        "./MFEDeliverableDetailsModule": "./src/app/mfe-modules/mfe-deliverable-details.module.ts",
+        "./MFEDeliverableDiagramModule": "./src/app/mfe-modules/mfe-deliverable-diagram.module.ts",
+        "./MFEDeliverableDetailsComponent": "./src/app/mfe-modules/mfe-deliverable-details/mfe-deliverable-details-sa.component.ts"
+      },
+      shared: share({
+        ...shareAll({ singleton: true, strictVersion: false, requiredVersion: "auto" }),
+        ...sharedMappings.getDescriptors()
+      })
+
+    }),
+    sharedMappings.getPlugin()
+  ],
+};
